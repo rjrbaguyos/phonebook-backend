2176e9a04eac5dab07dac5c748116ee77c26d37b
amamiyasiren <srn59937@gmail.com>
2026-09-13 16:32:15 -0800

    uibuild

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error("error.message")
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
